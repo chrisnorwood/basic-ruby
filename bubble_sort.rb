16e5eeaa7fc9fa202042be32fe633cfb916795da
Chris Norwood <cnorwood92@gmail.com>
2016-05-07 17:06:17 -0700

    bubble_sort_by completed, and used to clean up bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,21 @@
-# Maybe not the most elegant solution, for loops and changing
+def bubble_sort source_array
+  
+  bubble_sort_by(array) { |left, right| left - right }
 
-def bubble_sort array
-  # Iterating through array as many times as its size
-  # ensures all big numbers will be "pushed" to the back
+  array
+end
+
+def bubble_sort_by source_array, &block
   (array.length).times do
-    
-    # going from index 0 through (array.length-2) means we will 
-    # stop comparing "next" value, when "next" is the last element
-    for i in 0..array.length-2
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+    array.each_with_index do |value, index|
+      # checks if next element in array exists, and
+      # if code block returns number greater than 0 (which
+      # implies that the list is still unsorted)
+      if array[index+1] && yield(array[index], array[index+1]) > 0
+        array[index], array[index+1] = array[index+1], array[index]
       end
     end
   end
-
+  
   array
 end
